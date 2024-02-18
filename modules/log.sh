2d54c2d119b9c25c0300bfdd8771fb4886b713ae
georglauterbach <44545919+georglauterbach@users.noreply.github.com>
2024-02-18 14:50:06 +0100

    fix colors and simplyfy internal logger

--- modules/log.sh
+++ modules/log.sh
@@ -5,11 +5,11 @@
 # task          provides logging functionality
 
 export LIBBASH__LOG_COLOR_RESET='\e[0m'
-export LIBBASH__LOG_COLOR_TRA='\e[92m'
-export LIBBASH__LOG_COLOR_DEB='\e[36m'
-export LIBBASH__LOG_COLOR_INF='\e[34m'
-export LIBBASH__LOG_COLOR_WAR='\e[93m'
-export LIBBASH__LOG_COLOR_ERR='\e[91m'
+export LIBBASH__LOG_COLOR_TRACE='\e[92m'
+export LIBBASH__LOG_COLOR_DEBUG='\e[36m'
+export LIBBASH__LOG_COLOR_INFO='\e[34m'
+export LIBBASH__LOG_COLOR_WARN='\e[93m'
+export LIBBASH__LOG_COLOR_ERROR='\e[91m'
 
 if [[ ! -v __LIBBASH__IS_LOADED_LOG ]]
 then
@@ -33,15 +33,13 @@ fi
 # $2 :: message (strictly speaking optional, no default / empty string)
 function log() {
   function __log_generic() {
-    local LOG_LEVEL_ABBREVIATION=${1:?Log level abbreviation must be provided to __log_generic}
-    local LOG_LEVEL=${2:?Log level message format must be provided to __log_generic}
-    shift 2
-
-    local COLOR="LIBBASH__LOG_COLOR_${LOG_LEVEL_ABBREVIATION^^}"
+    local LOG_LEVEL=${1:?Log level message format must be provided to __log_generic}
+    local COLOR="LIBBASH__LOG_COLOR_${LOG_LEVEL^^}"
+    shift 1
 
     # shellcheck disable=SC2059
     printf "${!COLOR}%s${LIBBASH__LOG_COLOR_RESET}  %s  ${!COLOR}%s${LIBBASH__LOG_COLOR_RESET}  %s\n" \
-      "${LOG_LEVEL}" "$(date --iso-8601='seconds')" "${SCRIPT:-${0}}" "${*}"
+      "${LOG_LEVEL^^}" "$(date --iso-8601='seconds')" "${SCRIPT:-${0}}" "${*}"
   }
 
   # Log Level
@@ -61,7 +59,7 @@ function log() {
   local LOG_LEVEL_AS_INTEGER=2 MESSAGE_LOG_LEVEL="${1:-}"
   shift 1
 
-  case "${LOG_LEVEL:-inf}"
+  case "${LOG_LEVEL:-info}"
   in
     ( 'error' ) LOG_LEVEL_AS_INTEGER=0 ;;
     ( 'warn'  ) LOG_LEVEL_AS_INTEGER=1 ;;
@@ -79,26 +77,26 @@ function log() {
   in
     ( 'trace' )
       [[ ${LOG_LEVEL_AS_INTEGER} -lt 4 ]] && return 0
-      __log_generic 'trace' 'TRACE' "${*}"
+      __log_generic 'trace' "${*}"
       ;;
 
     ( 'debug' )
       [[ ${LOG_LEVEL_AS_INTEGER} -lt 3 ]] && return 0
-      __log_generic 'debug' 'DEBUG' "${*}"
+      __log_generic 'debug' "${*}"
       ;;
 
     ( 'info' )
       [[ "${LOG_LEVEL_AS_INTEGER}" -lt 2 ]] && return 0
-      __log_generic 'info' 'INFO ' "${*}"
+      __log_generic 'info' "${*}"
       ;;
 
     ( 'warn' )
       [[ "${LOG_LEVEL_AS_INTEGER}" -lt 1 ]] && return 0
-      __log_generic 'warn' 'WARN ' "${*}"
+      __log_generic 'warn' "${*}"
       ;;
 
     ( 'error' )
-      __log_generic 'error' 'ERROR' "${*}"
+      __log_generic 'error' "${*}"
       ;;
 
     ( * )
